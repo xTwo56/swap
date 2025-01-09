6251961ec72f643b697e6b3753b21f6434cf21ae
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-09 11:38:24 +0530

    ix make_offer added

--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -13,7 +13,6 @@ declare_id!("4gcFVNzdA4yjWsQvCh95tga3GUwHVLvPn1k1EM8dvUpn");
 
 #[program]
 pub mod swap {
-    use anchor_lang::solana_program::stake::instruction;
 
     use super::*;
 
@@ -25,9 +24,5 @@ pub mod swap {
     ) -> Result<()> {
         instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
         instructions::make_offer::save_offer(&mut ctx, id, token_b_wanted_amount)
-    }
-
-    pub fn take_offer(ctx: Context<TakeOffer>) -> Result<()> {
-        instruction::take_offer::send_wanted_tokens_to_maker(&ctx)?;
     }
 }

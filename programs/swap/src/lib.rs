4941e86e32d5e881432f49e9bad344323d57dd5e
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-21 16:46:25 +0530

    feat: ix take_offer added in lin.rs

--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -25,4 +25,9 @@ pub mod swap {
         instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
         instructions::make_offer::save_offer(&mut ctx, id, token_b_wanted_amount)
     }
+
+    pub fn take_offer(ctx: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&ctx)?;
+        instructions::take_offer::withdraw_and_close_vault(ctx)
+    }
 }

9a658b335db6104c381bf354b38bf0b4b495d114
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2025-01-21 16:47:57 +0530

    feat: visibilty streamlined

--- programs/swap/src/instructions/shared.rs
+++ programs/swap/src/instructions/shared.rs
@@ -3,7 +3,7 @@ use anchor_spl::token_interface::{
     transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked,
 };
 
-pub fn transfer_token<'info>(
+pub fn transfer_tokens<'info>(
     from: &InterfaceAccount<'info, TokenAccount>,
     to: &InterfaceAccount<'info, TokenAccount>,
     amount: &u64,
